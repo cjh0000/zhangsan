f33b3b59fe20d5fbc2593d8edefab5f122bd7cdb
JinHang <905430847@qq.com>
2020-09-14 15:19:47 +0800

    第一次向GitHub提交代码

---
+++ demon01.py
@@ -0,0 +1,19 @@
+# print(type("haha"))
+# print(type(2333))
+# print(type(2.33))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# a = str("2333")
+# print(type(a))
+
+# a = float(input("输入一个数字："))
+# b = float(input("输入一个数字："))
+# print("input输入的数字：",a+b)
+
+a = input("请输入字段1：")
+b = input("请输入字段2：")
+
+print(float(len(a)+len(b)),a+b)
